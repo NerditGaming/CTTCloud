1bf52309ac4af78894456e27d5e67d5f4b7eb583
NerditGaming <33577973+NerditGaming@users.noreply.github.com>
2017-11-12 11:35:36 +0000

    Primeira versao

---
+++ Can'ttouchthis 1/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 offset;
+	public float rotatespeed;
+	public Transform pivot;
+
+	// Use this for initialization
+	void Start () {
+		offset = target.position - transform.position;
+		pivot.transform.position = target.transform.position;
+		pivot.transform.parent = target.transform;
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		float horizontal = Input.GetAxis ("Mouse X") * rotatespeed;
+		target.Rotate (0, horizontal,0);
+
+		float vertical = Input.GetAxis ("Mouse Y") * rotatespeed;
+		pivot.Rotate (vertical, 0, 0);
+
+		float angulo_y = target.eulerAngles.y;
+		float angulo_x = pivot.eulerAngles.x;
+		Quaternion rotation = Quaternion.Euler (-angulo_x, angulo_y, 0);
+
+		transform.position = target.position - (rotation * offset);
+
+		//transform.position = target.position - offset;
+
+		transform.LookAt (target);
+	}
+}
---
+++ Can'ttouchthis 1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController cc;
+	public float moveSpeed;
+	public float jumpForce;
+	public float gravity;
+
+	private Vector3 direction = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+
+		cc = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		direction = (transform.forward * Input.GetAxis ("Vertical"))+(transform.right*Input.GetAxis("Horizontal"));
+		direction = direction.normalized * moveSpeed;
+
+		if (cc.isGrounded) {
+
+			//direction = new Vector3 (Input.GetAxis ("Horizontal") * moveSpeed, 0, Input.GetAxis ("Vertical") * moveSpeed);
+
+			//direction = transform.TransformDirection (direction);
+
+			//direction = direction * moveSpeed;
+
+			direction.y = 0f;
+
+			if (Input.GetButtonDown ("Jump")) {
+				direction.y = jumpForce;
+			}
+
+		}
+
+
+		direction.y = direction.y + (Physics.gravity.y * gravity * Time.deltaTime);
+
+		cc.Move (direction * Time.deltaTime);
+
+	}
+	
+}
